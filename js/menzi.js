6e1136530d41f992782a61f430c5ee3a041c570c
yubaoquan <1079139460@qq.com>
2014-10-27 23:33:08 +0800

    继续完善;
    添加滚动条;
    添加聊天框;
    句子背景色区分;

--- js/menzi.js
+++ js/menzi.js
@@ -3,8 +3,10 @@ menziWords[0] = "什么心态";
 menziWords[1] = "...";
 
 var func1 = function() {
-	var sentence=document.getElementById("iSay").value;
+	var whatUSay=document.getElementById("iSay").value;
 	var content = document.getElementById("content");
-	content.innerHTML += "<p>" + sentence + "</p>"
-	content.innerHTML += "<p>" + menziWords[0] + "</p>"
+	whatUSay = "<p class=\"singleSentence\">你对闷子说:<br>" + whatUSay+"</p>";
+	content.innerHTML += whatUSay;
+	content.innerHTML += "<p class=\"peiliaoSingleSentence\">闷子对你说:<br>" + menziWords[0] + "</p>"
+	content.scrollTop = content.scrollHeight;
 }
