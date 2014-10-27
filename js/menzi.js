f9e5d9e5325bac15759d75ba5ace0bd90c95828b
yubaoquan <721731428@qq.com>
2014-10-27 16:50:36 +0800

    闷子说话;

--- js/menzi.js
+++ js/menzi.js
@@ -1,12 +1,10 @@
+var menziWords = new Array();
+menziWords[0] = "什么心态";
+menziWords[1] = "...";
+
 var func1 = function() {
 	var sentence=document.getElementById("iSay").value;
-	//alert(sentence);
-	
-	var p = document.createElement("p");
-	var node=document.createTextNode(sentence);
-	p.appendChild(node);
 	var content = document.getElementById("content");
-	content.appendChild(p);
-	//content.innerHTML = content.innerHTML + "<p>" + sentence + "</p>"
-	//alert("ok2");
+	content.innerHTML += "<p>" + sentence + "</p>"
+	content.innerHTML += "<p>" + menziWords[0] + "</p>"
 }
