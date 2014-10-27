33a03a7a04a2d4c550c7bd66752a3c04920802dd
yubaoquan <721731428@qq.com>
2014-10-27 16:20:05 +0800

    记录对话内容2

--- js/menzi.js
+++ js/menzi.js
@@ -1,6 +1,12 @@
 var func1 = function() {
-	var sentence=document.getElementById("iSay").innerHTML;
-	alert(sentence);
-	var contentArea = document.getElementById("content");
-	contentArea.value += <p>sentence</p>;
+	var sentence=document.getElementById("iSay").value;
+	//alert(sentence);
+	
+	var p = document.createElement("p");
+	var node=document.createTextNode(sentence);
+	p.appendChild(node);
+	var content = document.getElementById("content");
+	content.appendChild(p);
+	//content.innerHTML = content.innerHTML + "<p>" + sentence + "</p>"
+	//alert("ok2");
 }
