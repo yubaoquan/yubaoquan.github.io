3b3c1daae08909d991ec2432863f4c1159b2baa9
yubaoquan <1079139460@qq.com>
2014-10-28 22:09:09 +0800

    在宿舍;
    1.添加陪聊头像;
    2.修改背景颜色;
    3.添加更多话语,但是每个陪聊只能说第一句;
    4.添加每个人的欢迎语;
    5.修改样式;

--- js/menzi.js
+++ js/menzi.js
@@ -2,18 +2,49 @@ var menziWords = new Array();
 menziWords[0] = "什么心态";
 menziWords[1] = "...";
 
+var yujiongWords = new Array();
+yujiongWords[0] = "摸摸头";
+yujiongWords[1] = "老娘抽死你丫的!";
+yujiongWords[2] = "→_→";
+
+var wuyaWords = new Array();
+wuyaWords[0] = "卧槽!";
+wuyaWords[1] = "我勒个去!";
+
+var chaterWords = new Object();
+chaterWords["zhoumenzi"] = menziWords;
+chaterWords["yujiong"] = yujiongWords;
+chaterWords["wuya"] = wuyaWords;
+
+var chaterNames = new Object();
+chaterNames["zhoumenzi"] = "周闷子";
+chaterNames["yujiong"] = "玉扃";
+chaterNames["wuya"] = "乌小六";
+
 var hello = new Object();
 hello["zhoumenzi"] = "你好,我是周闷子 ⊙ˍ⊙ 很高兴陪你聊天";
-hello["yujiong"] = "(╯‵□′)╯︵┻━┻";
-var selectedChater = document.getElementById("zhoumenzi");
+hello["yujiong"] = "我就是热情如火的玉扃!(╯‵□′)╯︵┻━┻";
+hello["wuya"] = "卧槽!卧槽!hhhhuuuooo!"
+var selectedChater;
 
 var func1 = function() {
 	var whatUSay=document.getElementById("iSay").value;
 	var content = document.getElementById("content");
-	whatUSay = "<p class=\"singleSentence\">你对闷子说:<br>" + whatUSay+"</p>";
+	var chaterName;
+	var chaterID;
+	if (typeof(selectedChater) == "undefined") {
+		chaterName = chaterNames["zhoumenzi"];
+		chaterID = "zhoumenzi";
+	} else {
+		chaterName = chaterNames[selectedChater.id];
+		chaterID = selectedChater.id;
+	}
+	whatUSay = "<p class=\"singleSentence\"><b>你</b>对<b>" + chaterName + "</b>说:<br>" + whatUSay+"</p>";
 	content.innerHTML += whatUSay;
-	content.innerHTML += "<p class=\"peiliaoSingleSentence\">闷子对你说:<br>" + menziWords[0] + "</p>"
+	var words = chaterWords[chaterID];
+	content.innerHTML += "<p class=\"peiliaoSingleSentence\"><b>" + chaterName + "</b>对<b>你</b>说:<br>" + words[0] + "</p>"
 	content.scrollTop = content.scrollHeight;
+	document.getElementById("iSay").value = "";
 }
 
 function BindEnter(obj){
@@ -27,7 +58,8 @@ function BindEnter(obj){
 function selectChater(event) {
 	//alert(event.innerHTML);
 	if (selectedChater != null) {
-		selectedChater.style.backgroundColor="white";
+		var bodyColor = document.bgColor;
+		selectedChater.style.backgroundColor=bodyColor;
 	}
 	
 	var chaterID = event.id;
