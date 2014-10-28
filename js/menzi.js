7ff9acf0ae59e17afb8ed46e8a33c14f0d9a6360
yubaoquan <721731428@qq.com>
2014-10-28 14:55:23 +0800

    可以选择陪聊;

--- js/menzi.js
+++ js/menzi.js
@@ -2,6 +2,11 @@ var menziWords = new Array();
 menziWords[0] = "什么心态";
 menziWords[1] = "...";
 
+var hello = new Object();
+hello["zhoumenzi"] = "你好,我是周闷子 ⊙ˍ⊙ 很高兴陪你聊天";
+hello["yujiong"] = "(╯‵□′)╯︵┻━┻";
+var selectedChater = document.getElementById("zhoumenzi");
+
 var func1 = function() {
 	var whatUSay=document.getElementById("iSay").value;
 	var content = document.getElementById("content");
@@ -9,4 +14,25 @@ var func1 = function() {
 	content.innerHTML += whatUSay;
 	content.innerHTML += "<p class=\"peiliaoSingleSentence\">闷子对你说:<br>" + menziWords[0] + "</p>"
 	content.scrollTop = content.scrollHeight;
+}
+
+function BindEnter(obj){
+	//alert("ok");
+	var button = document.getElementById('ok');
+	if(obj.keyCode == 13){
+		button.click();
+	}
+}
+
+function selectChater(event) {
+	//alert(event.innerHTML);
+	if (selectedChater != null) {
+		selectedChater.style.backgroundColor="white";
+	}
+	
+	var chaterID = event.id;
+	var helloSentence = hello[chaterID];
+	document.getElementById("hello").innerHTML = helloSentence;
+	event.style.backgroundColor="#1ec5e5"
+	selectedChater = event;
 }
