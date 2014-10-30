f248b1f6aacbfa8b403be39272a4dcb75f3a49ca
yubaoquan <721731428@qq.com>
2014-10-30 16:16:48 +0800

    在公司,又是无事可做的一天;
    1.优化页面展示效果;
    2.重构,每个陪聊具备自己的属性和方法;
    3.constant.js更名为variable.js;
    4.部分变量重命名;
    5.添加CKEditor作为可选的文本编辑界面;
    6.修正了Enter作为发送快捷键时按Ctrl+Enter文本不回车的问题;
    
    待解决:按Enter发送之后,textarea里会残余一个换行符;
    待添加:
    1.新角色;
    2.角色根据情绪说不同的话;
    ...

--- js/menzi.js
+++ js/menzi.js
@@ -1,105 +1,104 @@
-var func1 = function() {
-	var whatUSay=document.getElementById("iSay").value;
-	var content = document.getElementById("content");
-	var chaterName;
-	var chaterID;
-	if (typeof(selectedChater) == "undefined") {
-		chaterName = chaterNames["zhoumenzi"];
-		chaterID = "zhoumenzi";
-	} else {
-		chaterName = chaterNames[selectedChater.id];
-		chaterID = selectedChater.id;
-	}
-	//whatUSay = whatUSay.replace(new RegExp(">","g"), "&gt").replace(new RegExp("<","g"), "&lt");
-	whatUSay = whatUSay.replace(/[\r\n]/g,"<BR>").replace(/[\n]/g,"<BR>");
-
-	console.log(whatUSay);
-
-	var whatUSayWapper = "<p class=\"singleSentence\"><b>你</b>对<b>" + chaterName + "</b>说:<br></p>" + whatUSay;
-	// whatUSay = GetContents();
-	content.innerHTML += whatUSayWapper;
-	var words = chaterWords[chaterID];
-	var wordIndex = rdm(words.length);
-	if (chaterID == "yujiong") {
-		var sex = document.getElementById("sex").value;
-		//alert(sex);
-		var middle = parseInt(words.length / 2);
-		wordIndex = rdm(middle);
-		if (sex == "male") {
-			alert("male");
-			wordIndex += middle;
-			alert(wordIndex);
-		} 
-	}
-	//console.log(wordIndex);
-	var peiliaoWord = words[wordIndex];
-	if (wordIndex == 2) {
-		if (whatUSay.length >=2)  {
-			peiliaoWord = whatUSay.substr(whatUSay.length - 2)+ peiliaoWord;
-		} else {
-			peiliaoWord = "额";
-		}
-		
-	}
-	var peiliaoWordsWapper = "<p class=\"peiliaoSingleSentence\"><b>" + chaterName + "</b>对<b>你</b>说:<br></p>" + peiliaoWord;
-	content.innerHTML += peiliaoWordsWapper;
-	content.scrollTop = content.scrollHeight;
-	document.getElementById("iSay").value = "";
+//陪聊根据用户所说的话进行回应;
+function talk() {
+	var whatUSayInfo = GetContents();
+	var whatUSay = whatUSayInfo.text;
+	var whatPeiliaoSay = currentPeiliao.say(whatUSay);
+	refreshContent(whatUSayInfo.wrapper, whatPeiliaoSay);
 }
 
+//发送消息后,更新聊天记录,情况编辑界面;
+function refreshContent(whatUSayWrapper, peiliaoWord) {
+	//whatUSayWrapper = whatUSayWrapper.replace("<p", "<p class=\"whatUSay\"");
+	var content = document.getElementById("content");
+	content.innerHTML += "<p class=\"singleSentence\"><b>你:</b></p>";
+	content.innerHTML += whatUSayWrapper;
+	content.innerHTML += "<p class=\"peiliaoSingleSentence\"><b>" + currentPeiliao.name + "</b>:</p>";
+	content.innerHTML += "<p class=\"peiliaoWord\">" + peiliaoWord +"</p>";
+	content.scrollTop = content.scrollHeight;
+	clearTextArea();
+	console.log(whatUSayWrapper);
+	//alert(document.getElementById("iSay").value.length);
+}
+
+//监听回车和ctrl+回车事件,实现使用textarea作为编辑界面时的发送和换行功能;
 function BindEnter(obj){
-	//alert("ok");
 	var button = document.getElementById('ok');
 	if (sendMethod == "ce") {
 		if(obj.ctrlKey&&obj.keyCode == 13){
 			button.click();
 		}
-	} else {
-		//alert(sendMethod);
-		if(!obj.ctrlKey&&obj.keyCode == 13){
-			button.click();
+	} else if (sendMethod == "en"){
+		if(obj.keyCode == 13){
+			if (!obj.ctrlKey) {
+				button.click();
+			} else {
+				document.getElementById("iSay").value += "\n";
+			}
 		}
+	} else {
+		alert("wtf");
 	}
 	
 }
 
-function selectChater(event) {
+//选择陪聊;
+function selectChater(element) {
 	var bodyColor = document.bgColor;
-	selectedChater.style.backgroundColor=bodyColor;
+	currentPeiliaoElement.style.backgroundColor=bodyColor;
 
-	var chaterID = event.id;
-	if (chaterID == "yujiong") {
+	currentPeiliaoID = element.id;
+	currentPeiliao = peiliaoList[element.id];
+	if (currentPeiliaoID == "yujiong") {
 		var sex = getSex();
 		alert(sex);
 		document.getElementById("sex").value = sex;
 	}
-	// var chaterImg = document.getElementById("a");
-	// var imgSrc = "../images/" + chaterID + ".jpg";
-	// chaterImg.src = imgSrc;
-	var imgStr = "<img id=\"a\" src=\"../images/"+ chaterID +".jpg\"/>";
+	
+	var imgFilePath = "../images/" + currentPeiliaoID + ".jpg";
+	var imgElem = "<img id=\"a\" src=\" "+ imgFilePath+"\"/>";
 
-	var helloSentence = hello[chaterID];
-	document.getElementById("hello").innerHTML = imgStr + helloSentence;
-	event.style.backgroundColor="#1ec5e5"
-	selectedChater = event;
+	document.getElementById("hello").innerHTML = imgElem + currentPeiliao.hello;
+	element.style.backgroundColor="#1ec5e5"
+	currentPeiliaoElement = element;
 }
 
+//界面打开时的初始化工作;
 function pageLoad() {
-	selectedChater = document.getElementById("zhoumenzi");
-	selectChater(selectedChater);
+	currentPeiliaoElement = document.getElementById("zhoumenzi");
+	selectChater(currentPeiliaoElement);
 	sendMethod = "ce";
 	//abc();
 }
 
+//生成[0,n)之间的随机数;
 function rdm(n) {
 	return Math.floor(Math.random()*n);
 }
 
+//从编辑界面中获取消息,包含消息的纯文本和带样式的文本;;
 function GetContents() {
-	// Get the editor instance that you want to interact with.
-	var editor = CKEDITOR.instances.editor1;
-	// Get editor contents
-	return editor.getData();
+	var whatUSayInfo = new Object();
+	if (sendMethod == "ck") {
+		var editor = CKEDITOR.instances.iSay;
+		whatUSayInfo.wrapper = editor.getData();
+		whatUSayInfo.text = editor.document.getBody().getText();
+	} else {
+		var whatUSay=document.getElementById("iSay").value;
+		whatUSayInfo.wrapper = whatUSay.replace(/[\r\n]/g,"<br>").replace(/[\n]/g,"<br>")
+		whatUSayInfo.text = whatUSay;
+	}
+	whatUSayInfo.wrapper = "<div class=\"whatUSay\"> "+ whatUSayInfo.wrapper + "</div>";
+	return whatUSayInfo;
+}
+
+//用户发送消息之后清空编辑框;
+function clearTextArea() {
+	if (sendMethod == "ck") {
+		var editor = CKEDITOR.instances.iSay;
+		editor.setData("");
+	} else {
+		document.getElementById("iSay").value = "";
+	}
 }
 
 function getSex() {
@@ -109,7 +108,27 @@ function getSex() {
 	}
 }
 
+//更改发送消息的快捷键方式,包含更改编辑界面,textarea和CKEditor之间互相转换;
 function changeSendMethod(obj) {
+	if (sendMethod == obj.value) {
+		return;
+	}
+	var textarea = document.getElementById("iSay");
+	if (obj.value == "ck") {
+		var ckeditor = document.getElementById("cke_iSay");
+		if (ckeditor == null) {
+			CKEDITOR.replace('editor1');
+		} else {
+			textarea.setAttribute("style", "visibility: hidden; display: none;");
+			ckeditor.setAttribute("style", "visibility: visible; display: inline;");
+		}
+	} else {
+		if (sendMethod == "ck") {
+			var ckeditor = document.getElementById("cke_iSay");
+			ckeditor.setAttribute("style", "visibility: hidden; display: none;");
+			textarea.setAttribute("style", "visibility: visible; display: inline;");
+		}
+	}
 	sendMethod = obj.value;
 	
 }
