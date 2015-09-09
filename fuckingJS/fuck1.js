eae04902b288969c0b04447d6d3cbef3e945b972
yubaoquan <1079139460@qq.com>
2015-09-09 21:52:30 +0800

    09092151
    是不是每个人都像我一样艰难?
    什么地方, 静静躺着, 他的坚强;
    什么地方, 悄悄隐藏, 那段悲伤... ...

--- fuckingJS/fuck1.js
+++ fuckingJS/fuck1.js
@@ -1,5 +1,6 @@
 (function($) {
-    var i, len = window.dayByDayRecord.length;
+    var i = 0, len = window.dayByDayRecord.length;
+    
     for(; i < len; i ++) {
         $('.day-by-day').append('<div>' + window.dayByDayRecord[i] + '</div>');
     }
