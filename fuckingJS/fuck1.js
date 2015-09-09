072e4096e746253f9a58a773eeb11c9b78d50402
yubaoquan <1079139460@qq.com>
2015-09-09 22:17:36 +0800

    09092217

--- fuckingJS/fuck1.js
+++ fuckingJS/fuck1.js
@@ -2,7 +2,7 @@
     var i = 0, len = window.dayByDayRecord.length;
 
     for(; i < len; i ++) {
-        $('.day-by-day div').append('<div>' + window.dayByDayRecord[i] + '</div>');
+        $('.day-by-day>div').append('<div>' + window.dayByDayRecord[i] + '</div>');
     }
 
 }(jQuery));
