f971d1eaa077d1fa0033905d8ff4fb8ef89f654c
yubaoquan <hzyubaoquan@corp.netease.com>
2017-05-12 01:09:07 +0800

    Encoder

---
+++ encoder/script.js
@@ -0,0 +1,65 @@
+(function() {
+    let textarea = document.querySelector('textarea');
+    let input = document.querySelector('input');
+    let btnEnc = document.querySelector('#btn-enc');
+    let btnDec = document.querySelector('#btn-dec');
+    const seed = 2;
+
+    function encrypt(text = '', key = '') {
+        text = convert2Ascii(text);
+        key = convert2Ascii(key);
+        while (key.length < text.length) {
+            key += key;
+        }
+        key = key.split('');
+        text =  text.split('').map((char, index) => {
+            let sum = char.charCodeAt(0) + key[index].charCodeAt(0);
+            return String.fromCharCode('0x' + sum);
+        }).join('');
+        return btoa(encodeURIComponent(text));
+    }
+
+    function convert2Ascii(str) {
+        return btoa(encodeURIComponent(str));
+    }
+
+    function decrypt(text = '', key = '') {
+        try {
+            text = decodeURIComponent(atob(text));
+            let codePoints = [];
+            key = convert2Ascii(key);
+            for (let char of text) {
+                let num = parseInt(char.codePointAt(0));
+                codePoints.push(num.toString(16));
+            }
+            while (key.length < codePoints.length) {
+                key += key;
+            }
+            text = codePoints.map((n, index) => {
+                let codePoint = n - key[index].charCodeAt(0);
+                return String.fromCharCode(codePoint);
+            }).join('');
+            return decodeURIComponent(atob(text));
+        } catch (e) {
+            alert('Wrong key, decode fail.');
+            return '';
+        }
+    }
+
+    btnEnc.addEventListener('click', () => {
+        let text = textarea.value;
+        let key = input.value;
+        let result = encrypt(text, (key));
+
+        textarea.value = result;
+
+    });
+
+    btnDec.addEventListener('click', () => {
+        let text = textarea.value;
+        let key = input.value;
+        let result = decrypt(text, (key));
+
+        textarea.value = result;
+    });
+}());
